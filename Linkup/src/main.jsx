1f3dd3a489ba8d6739d5d1e31bd860a322e7a0b7
Sebastian <sebcyde@hotmail.com>
2023-03-05 19:34:28 +0000

    made styling folders

--- Linkup/src/main.jsx
+++ Linkup/src/main.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import './Styles/Globals/Global.scss';
+import React from 'react';
+import App from './App';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Route from './Routes/Route';
 import ErrorPage from './Routes/ErrorPage';
 
-
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Route />,
-    errorElement: <ErrorPage />,
-  },
+	{
+		path: '/',
+		element: <Route />,
+		errorElement: <ErrorPage />,
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
