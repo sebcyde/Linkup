aa46c460b184daa0bb1f9d621b3d372537bff57a
Sakariye Awale <99921216+sak45@users.noreply.github.com>
2023-03-09 20:32:34 +0000

    update error page and signOut

--- Linkup/src/pages/Settings/Settings.jsx
+++ Linkup/src/pages/Settings/Settings.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+
 
 const Settings = () => {
+	
 	return (
 		<div>
 			Settings
 			<Link to={'/'}>Home</Link>
 			<Link to={'/signup'}>Sign Up</Link>
 			<Link to={'/login'}>Log In</Link>
+			<Link to={'/ErrorPage'}>Error</Link>
 		</div>
 	);
 };
