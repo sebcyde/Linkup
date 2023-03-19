11f232dd62a8d9b3bd99715dad661389fc7ee303
Sebastian <sebcyde@hotmail.com>
2023-03-19 22:01:51 +0000

    Saks edits

--- Linkup/src/components/Home/LeftSide.jsx
+++ Linkup/src/components/Home/LeftSide.jsx
@@ -1,77 +1,39 @@
-import React from 'react'
-import HomeIcon from '@mui/icons-material/Home';
-import PostAddIcon from '@mui/icons-material/PostAdd';
-import MessageIcon from '@mui/icons-material/Message';
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-import EventIcon from '@mui/icons-material/Event';
-import VideocamIcon from '@mui/icons-material/Videocam';
-import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
-import FilePresentIcon from '@mui/icons-material/FilePresent';
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 export default function LeftSide() {
+  const [Loading, setLoading] = useState(true)
+  const [data, setData] = useState()
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://api.jikan.moe/v4/top/anime');
+      setData(response.data);
+      setLoading(false)
+      console.log('api call', (response.data))
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='leftSide-wrapper'>
         <h1 className='header'>
-            Menu 
+            Top Anime 
         </h1>
-        <ul className='section-list'>
-          <li>
-            <HomeIcon sx={{ fontSize: 30 }} /> 
-            <p className='list-text' >Home</p>
-          </li>
-          <li>
-            <PostAddIcon sx={{ fontSize: 30 }}  /> 
-            <p className='list-text'>Posts</p>
-          </li>
-          <li>
-            <MessageIcon sx={{ fontSize: 30 }} />
-            <p className='list-text'>Messages</p>
-          </li>
-          <li>
-            <PeopleAltIcon sx={{ fontSize: 30 }} /> 
-            <p className='list-text'>Friends</p>
-          </li>
-          <li>
-          <EventIcon sx={{ fontSize: 30 }} /> 
-          <p className='list-text'>Events</p>
-          </li>
-          <li>
-            <VideocamIcon sx={{ fontSize: 30 }} />
-            <p className='list-text'>Video</p>
-          </li>
-          <li>
-            <PhotoCameraIcon sx={{ fontSize: 30 }} />
-            <p className='list-text'>Photos</p>
-          </li>
-          <li>
-            <FilePresentIcon sx={{ fontSize: 30 }} />
-            <p className='list-text'>Files</p>
-          </li>
-          <li>
-            <SportsEsportsIcon sx={{ fontSize: 30 }} />
-            <p className='list-text'>Games</p>
-          </li>
-        </ul>
         <div className='line-thru'></div>
-        <h2 className='sub-header'>Trending</h2>
-        <div className='trend-section'>
-          <div className='trend-wrapper'>
-            <p className='trend-title'>Reality TV</p>
-            <h3 className='trend-header'>Maya</h3>
-            <p className='trend-posts'>59.8k Posts</p>
-          </div>
-          <div className='trend-wrapper'>
-            <p className='trend-title'>Politics</p>
-            <h3 className='trend-header'>Sturgeon</h3>
-            <p className='trend-posts'>5k Posts</p>
-          </div>
-          <div className='trend-wrapper'>
-            <p className='trend-title'>Sports</p>
-            <h3 className='trend-header'>Casemiro</h3>
-            <p className='trend-posts'>93.1k Posts</p>
-          </div>
+        <div className='anime-list-wrapper'>
+           {Loading ? '' : data.data.slice(0,5).map(anime => <div className='anime-card'  key={anime.title}>
+            <h3 className='anime-header'>{anime.title_english}</h3>
+            <img className='anime-img' src={anime.images.jpg.image_url} alt="" />
+            <div className='anime-details'>
+              <p className='anime-features'>{anime.synopsis}</p>
+            </div>
+            </div> )}
         </div>
     </div>
   )
-}
